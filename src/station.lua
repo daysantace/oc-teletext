e963122564f62afbf7a7f15dac33e4a5a331af46
daysantace@gmail.com <daysantace@gmail.com>
2022-11-28 19:00:40 +0000

    No

--- src/station.lua
+++ src/station.lua
@@ -14,19 +14,31 @@ textwr = ""
 
 term.clear()
 print("OC-teletext")
-print("Version 1.1.1")
+print("Version 1.1")
 print("Broadcasting text.")
 
 -- main loop
 
 while true do
   -- broadcast
-  os.sleep(5)
-
+  os.sleep(broadcastDelay)
   textwr = ""
-
   for line in io.lines("broadcast.txt") do
-    textwr = textwr .. line
+    textwr = textwr .. " ".. line
   end
+  
+  -- send colour
+  cB = ""
+  for line in io.lines("colourbg.txt") do
+    cB = cB.. " " .. line
+  end
+  
+  cF = ""
+  for line in io.lines("colourfg.txt") do
+    cF = cF .. " " .. line
+  end
+  
   modem.broadcast(port,textwr)
+  modem.broadcast(port,cB)
+  modem.broadcast(port,cF)
 end
--- src/tv.lua
+++ src/tv.lua
@@ -7,7 +7,7 @@ textReqCldwn = 30 -- time in secs to request text
 local event = require("event")
 local component = require("component")
 local colour = require("colors")
-local serialization = require("serialization")
+local serialise = require("serialization")
 local term = require("term")
 local gpu = component.gpu
 local modem = component.modem
@@ -15,18 +15,27 @@ local modem = component.modem
 gpu.setDepth(4)
 gpu.setResolution(80,25)
 
-gpu.fill(1,1,70,25," ")
+gpu.fill(1,1,80,25," ")
 term.setCursor(1,1)
 print("Waiting for broadcast...")
 
 -- main loop
 while true do
-    modem.open(port)
-    _,_,_,_,_,text=event.pull("modem_message")
-    _,_,_,_,_,cF=event.pull("modem_message")
-    _,_,_,_,_,cB=event.pull("modem_message")
+  txtT = nil
+  cbT = nil
+  cfT = nil
+  txtT = {}
+  cbT = {}
+  cfT = {}
+  modem.open(port)
+    _,_,_,_,_,txt = event.pull("modem_message")
+    _,_,_,_,_,cbR = event.pull("modem_message")
+    _,_,_,_,_,cfR = event.pull("modem_message")
     modem.close()
-
+    -- unserialise
+    for val in string.gmatch(txt, "%a+") do table.insert(txtT,val) end
+    for val in string.gmatch(cbR, "%a+") do table.insert(cbT,val) end
+    for val in string.gmatch(cfR, "%a+") do table.insert(cfT,val) end
     -- update text
     gpu.fill(1,1,80,25," ")
     gpu.setBackground(colour.white,true)
@@ -34,7 +43,13 @@ while true do
     gpu.fill(1,1,80,1," ")
     term.setCursor(1,1)
     print(tostring("OC-Teletext v1.0 - Page " .. tostring(page)))
-    gpu.setBackground(colour.black,true)
-    gpu.setForeground(colour.white,true)
-    term.write(text,true)
+    for i = 1, 20 do
+      text = txtT[i]
+      cB = cbT[i]
+      cF = cfT[i]
+      
+      gpu.setBackground(cB,true)
+      gpu.setForeground(cF,true)
+      print(text)
+    end
 end
